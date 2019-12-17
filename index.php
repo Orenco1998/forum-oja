4ae3e36d7e56b20ae18d20ac71d1b252916d41a0
Unknown <orencohen2652@gmail.com>
2019-12-17 15:39:20 +0100

    update index.php et subscirbe.php

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <head><link href="style.css" rel="stylesheet" media="all" type="text/css"> </head>
+<script>
+    var check = function() {
+        if (document.getElementById('password').value ==
+            document.getElementById('checkpassword').value) {
+            document.getElementById('message').style.color = 'green';
+            document.getElementById('message').innerHTML = 'matching';
+            document.getElementById('submit').disabled = false;
+
+        } else {
+            document.getElementById('message').style.color = 'red';
+            document.getElementById('message').innerHTML = 'not matching';
+            document.getElementById('submit').disabled = true;
+
+        }
+    }
+</script>
 
 <header>
-    <img src="../Forum-Oja/img/logo.png" alt= "Logo" id="logo"/>
+    <img src="../forum-oja/img/logo.png" alt= "Logo" id="logo"/>
 </header>
 
 <div class="colGlobal">
@@ -17,13 +33,15 @@
 <div class="col">
     <h2>S'inscrire</h2>
     <form action="subscribe.php" method="post">
-        <div><p>Nom : <input type="text" name="name" /></p></div>
-        <div><p>Prénom : <input type="text" name="firstname" /></p></div>
-        <div><p>Pseudo : <input type="text" name="nickname" /></p></div>
-        <div><p>Adresse Mail : <input type="text" name="email" /></p></div>
-        <div><p>Mot de Passe : <input type="text" name="password" /></p></div>
-        <div><p>Vérification du Mot de Passe : <input type="text" name="checkpassword" /></p></div>
-        <p><input class="button" type="submit" value="S'Inscrire"></p>
+        <div><p>Nom : <input type="text" name="name" required /></p></div>
+        <div><p>Prénom : <input type="text" name="firstname" required /></p></div>
+        <div><p>Pseudo : <input type="text" name="nickname" required /></p></div>
+        <div><p>Adresse Mail : <input type="email" name="email"  required /></p></div>
+        <div><p>Mot de Passe : <input type="password" name="password" id="password" onkeyup='check()' required /></p></div>
+        <div><p>Vérification du Mot de Passe : <input type="password" name="checkpassword" id="checkpassword" onkeyup='check()' required /></p>
+            <span id='message'></span>
+        </div>
+        <p><input class="button" type="submit" id="submit" value="S'Inscrire"></p>
     </form>
 </div>
 </div>
--- subscribe.php
+++ subscribe.php
@@ -7,13 +7,12 @@ $firstname = $_POST['firstname'];
 $nickname = $_POST['nickname'];
 $email = $_POST['email'];
 $password = md5($_POST['password']);
-$checkPassword = $_POST['checkpassword'];
+$checkPassword = md5($_POST['checkpassword']);
 $action = $_POST['action'];
 
 
-    echo 'Votre mail à bien été envoyé, <a href="../forum-oja/index.php"> Se connecter';
 
-    $query = "INSERT INTO user (name, firstName, nickname, email, password, date) values ('".$name."','".$firstname."','".$nickname."','".$email."','".$password."')";
+    $query = "INSERT INTO user (name, firstName, nickname, email, password) values ('".$name."','".$firstname."','".$nickname."','".$email."','".$password."')";
     if(!($dbResult = mysqli_query($dbLink, $query)))
     {
         echo 'Erreur de requête<br/>';
@@ -22,8 +21,6 @@ $action = $_POST['action'];
 // Affiche la requête envoyée.
         echo 'Requête : ' . $query . '<br/>';
         exit();
+    }else{
+        echo '<a href="../forum-oja/index.php"> Se connecter';
     }
-
-else{
-    echo '<a href="../forum-oja/index.php">Erreur lors de l\'inscription';
-}
