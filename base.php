986f10f4a7f1263e51e8ca8686ad085d2552f45c
Unknown <orencohen2652@gmail.com>
2019-12-17 13:45:25 +0100

    update base.php

--- base.php
+++ base.php
@@ -3,8 +3,8 @@
 $user = 'oren_forum-oja';
 $password = 'Institutg4!';
 $host = 'mysql-oren.alwaysdata.net';
-$nom_base = 'oren_forum-oja';
+$name_database = 'oren_forum-oja';
 
-$dbLink = mysqli_connect($host, $user, $password, $nom_base)
+$dbLink = mysqli_connect($host, $user, $password, $name_database)
 or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
-$mysqli = new mysqli($host, $user, $password, $nom_base);
+$mysqli = new mysqli($host, $user, $password, $name_database);
