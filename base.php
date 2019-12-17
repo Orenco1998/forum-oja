23d477b38e662d7bd9298ff651b8c33e689514da
Unknown <orencohen2652@gmail.com>
2019-12-17 11:18:44 +0100

    ajout de fichier

---
+++ base.php
@@ -0,0 +1,10 @@
+<?php
+
+$user = 'oren_bdd';
+$password = 'Institutg4!';
+$host = 'mysql-oren.alwaysdata.net';
+$nom_base = 'oren_forum-oja';
+
+$dbLink = mysqli_connect($host, $user, $password, $nom_base)
+or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+$mysqli = new mysqli($host, $user, $password, $nom_base);
