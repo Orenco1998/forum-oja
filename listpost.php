41af82e8ea5ebf144ad58afe71a1eb03d74b7544
Anthonu Jouvert <powerfullxd@gmail.com>
2019-12-20 10:01:21 +0100

    ListPost.php

--- listpost.php
+++ listpost.php
@@ -1,4 +1,4 @@
-<head><link href="style.css" rel="stylesheet" media="all" type="text/css"> </head>
+ <head><link href="style.css" rel="stylesheet" media="all" type="text/css"> </head>
 
 <script src="https://cdn.ckeditor.com/ckeditor5/16.0.0/classic/ckeditor.js"></script>
 
@@ -6,6 +6,20 @@
     <img src="../forum-oja/img/logo.png" alt= "Logo" id="logopage"/>
 </headerpage>
 <div class="centercol">
+    <h2>Les Posts</h2>
+    <table summary="exemple de structure d'un tableau de données 2 lignes, 2 colonnes">
+        <caption>Tableau de données</caption>
+
+        <tr>
+            <td>Pseudo</td>
+            <td>Date</td>
+        </tr>
+
+        <tr>
+            <td>Infos</td>
+            <td>Message</td>
+        </tr>
+    </table>
     <h2>Envoyer un message</h2>
     <form action="send.php" method="post">
         <div><h3>Titre </h3><input type="text" size="90" maxlength="100" name="titremessage" /></div>
@@ -16,7 +30,6 @@
         <p><input class="button" type="submit" value="Envoyer"></p>
     </form>
 </div>
-<br>
 <script>
     ClassicEditor
         .create( document.querySelector( '#editor' ) )
