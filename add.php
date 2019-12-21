dd42fe923516f1caac570b4715299856bc72cae3
Unknown <orencohen2652@gmail.com>
2019-12-21 19:07:28 +0100

    update add.php and listpost.php with style.css

--- add.php
+++ add.php
@@ -1,1 +1,29 @@
 <?php
+
+session_start();
+if(isset($_SESSION['email']) && isset($_SESSION['password']))
+    {
+        include 'base.php';
+
+
+        $title = $_POST['title'];
+        $message = $_POST['editor'];
+        echo $message;
+        $query = 'SELECT user_id FROM user WHERE email=\'' . $_SESSION['email'] . '\' AND password =\'' . $_SESSION['password'] . '\'';
+        $res = mysqli_query( $dbLink,"$query");
+        while($tab = mysqli_fetch_array($res)){
+            $user_id = $tab['user_id'];
+        }
+
+        $query = 'INSERT INTO post (title, message, post_date, user_id) VALUES (\'' . $title . '\', \'' . $message . '\', \'' . date('Y-m-d') . '\', ' . $user_id . ')';
+        if (!($dbResult = mysqli_query($dbLink, $query))) {
+            header('location:listpost.php?error=1');
+        } else {
+            header('location:listpost.php');
+
+        }
+
+    }else{
+    header('location:index.php?error=3');
+}
+
--- listpost.php
+++ listpost.php
@@ -11,6 +11,19 @@
 <div> <a id="haut"></a>
  </div>
 
+     <?php
+    if($_GET['error'] == 1) { ?>
+        <div class="message">
+
+
+                <h1> Impossible d'ajouté votre message veuillez rééssayer.</h1>
+
+
+        </div>
+        <?php
+    }
+?>
+
      <nav>
          <div class="right">
              <ul>
@@ -18,50 +31,87 @@
              </ul>
          </div>
      </nav>
+
 <header>
     <img src="../forum-oja/img/logo.png" alt= "Logo" id="logopage"/>
 </header>
-<div class="centercol">
-    <h2>Les Posts</h2>
-    <table summary="exemple de structure d'un tableau de données 2 lignes, 2 colonnes">
-        <caption>Tableau de données</caption>
 
-        <tr>
-            <td class="tdpetit">Pseudo</td>
-            <td class="tdgrand">Date</td>
-        </tr>
 
-        <tr>
-            <td class="tdpetit">Infos</td>
-            <td class="tdgrand">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet sagittis congue. Pellentesque ut lorem vel odio interdum fringilla nec id mi. Fusce sodales neque nec consequat luctus. Cras et magna lorem. Aenean erat lorem, consequat sit amet convallis quis, commodo lacinia ante. Integer pretium, leo ut eleifend porta, est augue hendrerit arcu, in pharetra nulla metus et sapien. Suspendisse efficitur ornare viverra. In faucibus augue id faucibus finibus. Nulla id libero in purus aliquam dignissim. Integer ac felis a elit tincidunt luctus. Nullam nisl augue, dignissim vel lacinia eu, commodo at magna.
 
-                Aliquam ut nunc sed magna sodales facilisis a quis ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum in suscipit ante. In placerat suscipit velit quis venenatis. In vel nulla porttitor dui euismod condimentum. Maecenas at risus sit amet arcu accumsan sagittis sit amet nec libero. Integer imperdiet nisi vitae enim molestie, in auctor orci ultricies.
-
-        </tr>
-    </table>
-    <h2>Envoyer un message</h2>
-    <form action="send.php" method="post">
-        <div><h3>Titre </h3><input type="text" size="90" maxlength="100" name="titremessage" /></div>
-        <br><h3>Votre message </h3>
-        <div id="editor">
-            <textarea name="positive" rows=10 COLS=40 maxlength="2048"></textarea>
-        </div>
-        <p><input class="button" type="submit" value="Envoyer"></p>
-    </form>
-</div>
-<div class=ancadre>
-    <a href="#haut">haut de page</a>  
-</div>
-
+         <div class="centercol">
+             <h2>Les Posts</h2>
+             <table>
      <?php
+     require 'base.php';
+     $perPage = 7;
 
+     $number = "SELECT COUNT(*) AS total from post";
+     $resultat = mysqli_query($dbLink,"$number");
+     while ($tab = mysqli_fetch_array($resultat)) {
+         $total = $tab['total'];
+
+         $nbPage = ceil($total/$perPage);
+
+
+         if(isset($_GET['p'])&& !empty($_GET['p']) && ctype_digit($_GET['p']) == 1)
+         {
+             if($_GET['p']> $nbPage)
+             {
+                 $current = $nbPage;
+             }else{
+                 $current = $_GET['p'];
+             }
+
+         }else{
+             $current = 1;
+         }
+
+         $firstOfPage = ($current-1)*$perPage;
+
+         $query = "SELECT * FROM post INNER JOIN user ON user.user_id = post.user_id ORDER BY post_date DESC LIMIT $firstOfPage,$perPage";
+         $res = mysqli_query($dbLink, $query);
+     }
+
+     while ($tab = mysqli_fetch_array($res)) { ?>
+                 <tr>
+                     <td class="tdpetit">Pseudo: <?= $tab['pseudo'] ?> </td>
+                     <td class="tdgrand"><?= $tab['post_date'] ?></td>
+                 </tr>
+
+                 <tr>
+                     <td class="tdpetit">
+                         Prénom : <?= $tab['name'] ?> <br/>
+                         Nom : <?= $tab['firstname'] ?>
+                     </td>
+                     <td class="tdgrand"><?= $tab['message'] ?></td>
+                 </tr>
+                 <?php
+                 } ?>
+             </table>
+
+             <h2>Envoyer un message</h2>
+             <form action="add.php" method="post">
+                 <div><h3>Titre </h3><input type="text" size="90" maxlength="100" name="title"/></div>
+                 <br>
+                 <h3>Votre message </h3>
+                 <textarea id="editor" name="editor" rows=10 COLS=40 maxlength="2048"></textarea>
+                 <p><input class="button" type="submit" value="Envoyer"></p>
+             </form>
+         </div>
+         <div class=ancadre>
+             <a href="#haut">haut de page</a>
+         </div>
+
+         <?php
  }
  else{
      header('location:index.php?error=3');
  }
  ?>
 
-
+ <script>
+     CKEDITOR.replace('editor');
+ </script>
 <script>
     ClassicEditor
         .create( document.querySelector( '#editor' ) )
